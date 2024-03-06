db57cd3a15b8d3ab66f0568250c4c9e34defd2ff
ThomasK1991 <thomaskaprielian@gmail.com>
2024-03-06 13:57:56 +0000

    Question 2 Completed

--- part-1/array_delete.hpp
+++ part-1/array_delete.hpp
@@ -4,11 +4,27 @@
 #include <cstddef>
 #include <utility>
 #include <vector>
-
+#include "utils.hpp"
+#include <thread>
+#include <chrono>
 template <typename T>
 void array_delete(std::vector<T> &A, std::size_t index)
 {
     // WRITE YOUR CODE HERE
+    assert(index < A.size() || index>=0);
+    if (index == A.size()-1)
+    {
+        A.pop_back(); //We chill
+        return;
+    }
+    else{
+        for(int i = index; i <A.size()-1; i++)
+        {
+            A[i] = A[i+1];
+        }
+    }
+    A.pop_back();
+    return;
 }
 
 #endif // __array_delete__
