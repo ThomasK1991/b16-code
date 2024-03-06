c4f2278340d4eee4ff2e7f418d8cbbeb806b6380
ThomasK1991 <thomaskaprielian@gmail.com>
2024-03-06 16:06:55 +0000

    RPN Complete with overloading operators extension

--- part-1/stack_rpn.hpp
+++ part-1/stack_rpn.hpp
@@ -6,5 +6,37 @@
 #include <functional>
 
 // WRITE YOUR CODE HERE
+template <typename T> 
+void multiplies(Stack<T> &stack){
+        T number1 = stack.top();
+        stack.pop();
+        T number2 = stack.top();
+        stack.pop();
+        stack.push(number1*number2);
+    };
+
+template <typename T>
+void plus(Stack<T> &stack){
+        T number1 = stack.top();
+        stack.pop();
+        T number2 = stack.top();
+        stack.pop();
+        stack.push(number1+number2);
+    };
+
+template <typename T>
+Stack<T> &operator<<(Stack<T> &stack, const T &value)
+{
+    // WRITE YOUR CODE HERE
+    stack.push(value);
+    return stack;
+}
+
+template <typename T>
+Stack<T> &operator<<(Stack<T> &stack, void(*func)(Stack<T> &stack))
+{
+    func(stack);
+    return stack;
+}
 
 #endif // __stack_rpn__
